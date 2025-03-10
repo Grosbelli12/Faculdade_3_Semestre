44516b8bf2174e56241222da05d1b1439c8d0d22
Grosbelli112 <gustavogrosbelli11@gmail.com>
2025-03-10 20:12:55 -0300

    atualizado

--- src/Faculdade_3_Semestre/Aulas/Fibonacci.kt
+++ src/Faculdade_3_Semestre/Aulas/Fibonacci.kt
@@ -1,6 +1,6 @@
 package Faculdade_3_Semestre.Aulas
 
- fun main (){
+ /*fun main (){
      var n1 = 2
      var n2 = 1
      var aux = 0
@@ -15,4 +15,28 @@ package Faculdade_3_Semestre.Aulas
          aux = n2
      }
      println(n2)
- }
+ }*/
+
+
+fun main(){
+     val regex = Regex("^(?:[2-9]|\\d{2,})$")
+     var num: String
+     do {
+         println("Digite uma casa da sequencia de Fibonacci:")
+         num = readln()
+     }while(!regex.matches(num))
+
+     println(calcFibonacci(num.toInt()))
+}
+
+fun calcFibonacci(n: Int): Int {
+    if (n<= 1){
+        return n
+    }
+    return calcFibonacci(n-1) + calcFibonacci(n-2)
+}
+
+
+
+
+
