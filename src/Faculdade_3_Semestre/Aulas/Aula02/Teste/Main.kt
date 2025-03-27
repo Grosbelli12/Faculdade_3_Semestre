8139bde188d7964238b710f53fde43df6a4715a5
Grosbelli112 <gustavogrosbelli11@gmail.com>
2025-03-26 22:26:17 -0300

    feita as funções busca e validar

--- src/Faculdade_3_Semestre/Aulas/Aula02/Teste/Main.kt
+++ src/Faculdade_3_Semestre/Aulas/Aula02/Teste/Main.kt
@@ -2,10 +2,8 @@ package Faculdade_3_Semestre.Aulas.Aula02.Teste
 
 import Faculdade_3_Semestre.Aulas.Aula02.Dominio.Convidado
 
-val expressaoRegular = Regex("[0-4]")
+val expressaoRegular = Regex("[0-5]")
 
-// var Global
-var convidado: Convidado = Convidado()
 
 // Instância de um lista de mutável vazia
 var listaConvidados: MutableList<Convidado> = mutableListOf<Convidado>()
@@ -21,6 +19,7 @@ private fun menu() {
         println("2- LISTAR")
         println("3- EDITAR")
         println("4- EXCLUIR")
+        println("5- BUSCA")
         println("0- SAIR")
 
         val opcao = readln()
@@ -49,6 +48,11 @@ private fun menu() {
                     excluir()
                 }
 
+                5 -> {
+                    println("Buscando...")
+                    busca()
+                }
+
                 0 -> print("Saindo...")
             }
         } else {
@@ -63,7 +67,7 @@ private fun menu() {
 }
 
 private fun cadastrar() {
-
+    val convidado: Convidado = Convidado()
     print("Qual é o seu nome? ")
     //val nome = readln()
     convidado.nome = readln()
@@ -81,10 +85,7 @@ private fun cadastrar() {
 
 private fun listar(): String {
     var i = 0
-    if (listaConvidados.isEmpty()) {
-        println("Não há convidados")
-
-    } else {
+    if (validar()) {
         listaConvidados.forEach { convidado ->
             print(
                 "Posição ${i++} " +
@@ -92,48 +93,69 @@ private fun listar(): String {
                         " Presente: ${convidado.presente};\n " +
                         " Restrição:${convidado.alimentar};\n " +
                         " Vai ir a festa? ${convidado.presenca}\n; "
-            )
-        }
-    }
+            ) //FIM DO PRINT
+        } // FIM DO FOR EACH
+    }// FIM DO IF
     return "Listagem foi um sucesso "// retorna a função
-
-}
+} // FIM DA FUNÇÃO LISTAR
 
 private fun editar(): Boolean {
-    if (listaConvidados.isEmpty()) {
-        println("A lista está vazia")
-        return false
-    }
-    listar()
+    if (validar()) {
+        listar()
 
-    println("Digite a posição a ser editada:")
-    val posicao = readln().toInt()
+        println("Digite a posição a ser editada:")
+        val posicao = readln().toInt()
 
-    println("O convidado vai? S/N")
-    val resposta = readln().uppercase().trim()
-    when (resposta) {
-        "S" -> listaConvidados[posicao].presenca = true
-        "N" -> listaConvidados[posicao].presenca = false
+        println("O convidado vai? S/N")
+        val resposta = readln().uppercase().trim()
+
+        when (resposta) {
+            "S" -> listaConvidados[posicao].presenca = true
+            "N" -> listaConvidados[posicao].presenca = false
+        }
     }
+
+
     return true
 }
 
 private fun excluir(): Boolean {
-    if (listaConvidados.isEmpty()) {
-        println("A lista está vazia")
-        return false
+    if (validar()) {
+        println("Qual posição você deseja remover:")
+        val posicao = readln().toInt()
+        listaConvidados.removeAt(posicao)
+
+        /* convidado.nome = ""
+         convidado.alimentar = ""
+         convidado.presente = ""
+         convidado.presenca = false
+         println("Convidar excluido ")*/
+
+        listar()
+        println("Candidato removido")
     }
-    listar()
-    println("Qual posição você deseja remover:")
-    val posicao = readln().toInt()
-    listaConvidados.removeAt(posicao)
-
-    /* convidado.nome = ""
-     convidado.alimentar = ""
-     convidado.presente = ""
-     convidado.presenca = false
-     println("Convidar excluido ")*/
-
-    println("Candidato removido")
     return true
+}
+
+private fun busca() {
+    var i = 0 // indece da lista
+    println("Digite o nome da pessoa que você busca: ")
+    val busca = readln()
+    listaConvidados.forEach { convidado ->
+        //o contains busca por uma string dentro de uma outra string
+        if (convidado.nome.contains(busca)) {
+            println("Posição: $i, Nome: ${convidado.nome} Presente: ${convidado.presente} Restrição: ${convidado.alimentar} Presença: ${convidado.presenca}")
+        }
+        i++
+    }
+}
+
+private fun validar(): Boolean {
+    if (listaConvidados.isEmpty()) {
+        println("Lista vazia! Finalizando ")
+        return false
+    } else {
+        return true
+    }
+
 }
