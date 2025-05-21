65e0c6aecc67a37f023286655e47a1d89a826bc8
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-05-21 19:04:56 -0300

    começo das atividades de revisão

---
+++ src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Disciplinas.kt
@@ -0,0 +1,15 @@
+package Faculdade_3_Semestre.RevisaoParaSegundaProva
+
+fun main(){
+
+    println("Qual foi a nota de Pedrinho em  matemática? ")
+    val notaMat = readln().toDouble()
+
+
+        val matematica = mapOf(
+            notaMat to "Pedrinho"
+        )
+
+    println(matematica)
+
+}
