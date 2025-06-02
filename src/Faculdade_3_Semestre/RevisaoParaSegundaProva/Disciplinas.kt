d988cda74f3e2fcde5a201d8ef4bdce1147933ba
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-06-02 20:19:13 -0300

    começo das atividades de revisão

--- src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Disciplinas.kt
+++ src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Disciplinas.kt
@@ -1,6 +1,7 @@
 package Faculdade_3_Semestre.RevisaoParaSegundaProva
 
 fun main(){
+
         val matematica = mapOf(
             6.1 to "Pedrinho",
             4.0 to "Paulo",
@@ -22,8 +23,22 @@ fun main(){
             10.0 to "Pedrinho",
             8.5 to "Paulo",
             9.0 to "Carlos"
-         )
+        )
 
-    println(matematica,)
+        val historia = mapOf(
+            7.0 to "Pedrinho",
+            6.5 to "Paulo",
+            8.0 to "Carlos"
+        )
 
+
+    for ((chave,valor) in matematica ){
+        val aluno = matematica[chave]
+
+        if (chave >= 7.0){
+                println("$valor tirou $chave passou de ano")
+        }else(
+            println("$valor tirou $chave reprovou de ano")
+        )
+    }
 }
