a6ae888de82fb949aca76526b893a1681212428b
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-06-02 19:19:55 -0300

    começo das atividades de revisão

--- src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Disciplinas.kt
+++ src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Disciplinas.kt
@@ -7,8 +7,23 @@ fun main(){
             8.5 to "Carlos"
         )
 
-        val
+        val  ciencias = mapOf(
+            9.0 to "Pedrinho",
+            8.0 to "Paulo",
+            8.5 to "Carlos"
+        )
 
-    println(matematica)
+        val portugues = mapOf(
+            3.0 to "Pedrinho",
+            9.5 to "Paulo",
+            8.0 to "Carlos"
+        )
+        val educacao_fisica = mapOf(
+            10.0 to "Pedrinho",
+            8.5 to "Paulo",
+            9.0 to "Carlos"
+         )
+
+    println(matematica,)
 
 }
