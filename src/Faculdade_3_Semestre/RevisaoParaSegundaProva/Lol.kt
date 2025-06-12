1df72edc7b386fe8fe703845874ab0064ee95739
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2025-06-12 11:39:10 -0300

    ex do LOL  Otimizado

--- src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Lol.kt
+++ src/Faculdade_3_Semestre/RevisaoParaSegundaProva/Lol.kt
@@ -1,7 +1,5 @@
 package Faculdade_3_Semestre.RevisaoParaSegundaProva
 
-import java.sql.Time
-
 fun main() {
 
     val jogador1Status = mapOf(
@@ -10,7 +8,7 @@ fun main() {
         "Dano Sofrido Médio" to 40.0,
         "Distância Percorrida" to 20.0,
         "Numero De Mortes" to 5,
-        "Numero de Abatimentos" to 25
+        "Numero De Abatimentos" to 25
     )
 
     val jogador2Status = mapOf(
@@ -19,7 +17,7 @@ fun main() {
         "Dano Sofrido Médio" to 30.0,
         "Distância Percorrida" to 15.0,
         "Numero De Mortes" to 3,
-        "Numero de Abatimentos" to 15
+        "Numero De Abatimentos" to 15
     )
 
     val jogador3Status = mapOf(
@@ -28,7 +26,7 @@ fun main() {
         "Dano Sofrido Médio" to 25.0,
         "Distância Percorrida" to 10.0,
         "Numero De Mortes" to 2,
-        "Numero de Abatimentos" to 4
+        "Numero De Abatimentos" to 4
     )
 
     val jogador4Status = mapOf(
@@ -37,88 +35,46 @@ fun main() {
         "Dano Sofrido Médio" to 55.0,
         "Distância Percorrida" to 23.0,
         "Numero De Mortes" to 2,
-        "Numero de Abatimentos" to 8
+        "Numero De Abatimentos" to 8
     )
 
-    val time: Map<String, Map<String, Any>> = mapOf(
-        "jogador1Status" to jogador1Status,
-        "jogador2Status" to jogador2Status,
-        "jogador3Status" to jogador3Status,
-        "jogador4Status" to jogador4Status
+    val time = mapOf(
+        1 to jogador1Status,
+        2 to jogador2Status,
+        3 to jogador3Status,
+        4 to jogador4Status
     )
+    var danoTotal = 0.0
+    var danoMedioTotal = 0.0
+    var danoTotalSofrido = 0.0
+    var distanciaTotalPercorrida = 0.0
+    var totalDeMortes = 0.0
+    var totalDeAbatimentos = 0.0
 
 
-    val metrica1 = "Dano Total"
+    time.values.forEach { jogador ->
+        val danoJogador = jogador["Dano Total"] as Double
+        val danoMedio = jogador["Dano Médio"] as Double
+        val danoSofrido = jogador["Dano Sofrido Médio"] as Double
+        val distanciaPercorrida = jogador["Distância Percorrida"] as Double
+        val numeroDeMortes = jogador["Numero De Mortes"] as Int
+        val numeroDeAbatimentos = jogador["Numero De Abatimentos"] as Int
 
-    val danoTotalJogador1 = jogador1Status[metrica1] as Double ?: 0.0
-    val danoTotalJogador2 = jogador2Status[metrica1] as Double ?: 0.0
-    val danoTotalJogador3 = jogador3Status[metrica1] as Double ?: 0.0
-    val danoTotalJogador4 = jogador4Status[metrica1] as Double ?: 0.0
+         danoTotal += danoJogador
+         danoMedioTotal += danoMedio
+         danoTotalSofrido += danoSofrido
+         distanciaTotalPercorrida += distanciaPercorrida
+         totalDeMortes += numeroDeMortes
+         totalDeAbatimentos += numeroDeAbatimentos
 
-    val mediaDanoTotal = (danoTotalJogador1 + danoTotalJogador2 + danoTotalJogador3 + danoTotalJogador4) / 4
-
-
-    val metrica2 = "Dano Médio"
-
-    val danoMedioJogador1 = jogador1Status[metrica2] as Double ?: 0.0
-    val danoMedioJogador2 = jogador2Status[metrica2] as Double ?: 0.0
-    val danoMedioJogador3 = jogador3Status[metrica2] as Double ?: 0.0
-    val danoMedioJogador4 = jogador4Status[metrica2] as Double ?: 0.0
-
-
-    val mediaDanoMedio = (danoMedioJogador1 + danoMedioJogador2 + danoMedioJogador3 + danoMedioJogador4) / 4
-
-    val metrica3 = "Dano Sofrido Médio"
-
-    val danoSofridoMedioJogador1 = jogador1Status[metrica3] as Double ?: 0.0
-    val danoSofridoMedioJogador2 = jogador2Status[metrica3] as Double ?: 0.0
-    val danoSofridoMedioJogador3 = jogador3Status[metrica3] as Double ?: 0.0
-    val danoSofridoMedioJogador4 = jogador4Status[metrica3] as Double ?: 0.0
-
-    val mediaDanoSofrido =
-        (danoSofridoMedioJogador1 + danoSofridoMedioJogador2 + danoSofridoMedioJogador3 + danoSofridoMedioJogador4) / 4
-
-
-    val metrica4 = "Distância Percorrida"
-
-    val distanciaPercorridaJogador1 = jogador1Status[metrica4] as? Double ?: 0.0
-    val distanciaPercorridaJogador2 = jogador2Status[metrica4] as? Double ?: 0.0
-    val distanciaPercorridaJogador3 = jogador3Status[metrica4] as? Double ?: 0.0
-    val distanciaPercorridaJogador4 = jogador4Status[metrica4] as? Double ?: 0.0
-
-    val mediaDistanciaPercorrida =
-        (distanciaPercorridaJogador1 + distanciaPercorridaJogador2 + distanciaPercorridaJogador3 + distanciaPercorridaJogador4) / 4.0
-
-
-    val metrica5 = "Numero De Mortes"
-
-    // Cast to Double for consistent calculations, even if original is Int
-    val numeroDeMortesJogador1 = jogador1Status[metrica5] as? Int ?: 0
-    val numeroDeMortesJogador2 = jogador2Status[metrica5] as? Int ?: 0
-    val numeroDeMortesJogador3 = jogador3Status[metrica5] as? Int ?: 0
-    val numeroDeMortesJogador4 = jogador4Status[metrica5] as? Int ?: 0
-
-    val mediaNumeroDeMortes =
-        (numeroDeMortesJogador1 + numeroDeMortesJogador2 + numeroDeMortesJogador3 + numeroDeMortesJogador4) / 4.0
-
-
-    val metrica6 = "Numero de Abatimentos"
-
-    val numeroDeAbatimentosJogador1 = jogador1Status[metrica6] as? Int ?: 0
-    val numeroDeAbatimentosJogador2 = jogador2Status[metrica6] as? Int ?: 0
-    val numeroDeAbatimentosJogador3 = jogador3Status[metrica6] as? Int ?: 0
-    val numeroDeAbatimentosJogador4 = jogador4Status[metrica6] as? Int ?: 0
-
-    val mediaNumeroDeAbatimentos =
-        (numeroDeAbatimentosJogador1 + numeroDeAbatimentosJogador2 + numeroDeAbatimentosJogador3 + numeroDeAbatimentosJogador4) / 4.0
-
+    }
     println(
-        "A média dos status da equipe em cada status foi:\n$metrica1: $mediaDanoTotal" +
-                "\n$metrica2: $mediaDanoMedio s" +
-                "\n$metrica3: $mediaDanoSofrido s" +
-                "\n$metrica4: $mediaDistanciaPercorrida" +
-                "\n$metrica5: $mediaNumeroDeMortes" +
-                "\n$metrica6: $mediaNumeroDeAbatimentos"
+        "A média dos status da equipe em cada status foi:\n Dano Total: ${danoTotal / 4} " +
+                "\n Dano Médio : ${danoMedioTotal / 4}" +
+                "\n Dano Sofrido Médio: ${danoTotalSofrido / 4} s" +
+                "\n Distância Percorrida: ${distanciaTotalPercorrida / 4}" +
+                "\n Numero De Mortes: ${totalDeMortes / 4}" +
+                "\n Numero de Abatimentos: ${totalDeAbatimentos / 4}"
     )
 }
 
